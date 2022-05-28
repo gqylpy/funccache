91fa3205b9308f80bbfdbbaac0d959be25894664
Yongkang Zhu <zhuyk4@lenovo.com>
2022-05-29 00:46:39 +0800

    Rewrite, still in progress. The next day.

--- gqylpy_cache/g cache.py
+++ gqylpy_cache/g cache.py
@@ -31,16 +31,23 @@ import threading
 
 
 class GqylpyCache(type):
-    __shared_instance_cache__ = True
+    __shared_instance_cache__ = False
     __not_cache__ = []
 
     def __init__(cls, __name__: str, __bases__: tuple, __dict__: dict):
-        type.__init__(cls, __name__, __bases__, __dict__)
+        __not_cache__: list = __dict__.get('__not_cache__')
+
+        if __not_cache__:
+            cls.delete_repeated(__not_cache__)
+            cls.check_and_tidy_not_cache(__not_cache__, __dict__)
+            cls.delete_repeated(__not_cache__)
+
+        cls.__getattribute__ = __getattribute__(cls)
 
         if cls.__shared_instance_cache__:
             cls.__cache_pool__ = {}
 
-        cls.__getattribute__ = __getattribute__(cls)
+        type.__init__(cls, __name__, __bases__, __dict__)
 
     def __call__(cls, *a, **kw):
         ins: cls = type.__call__(cls, *a, **kw)
@@ -50,8 +57,83 @@ class GqylpyCache(type):
 
         return ins
 
+    def check_and_tidy_not_cache(cls, __not_cache__: list, __dict__: dict):
+        error_methods = []
+
+        for index, name_or_method in enumerate(__not_cache__):
+            if name_or_method.__class__ is str:
+                name: str = name_or_method
+
+                if name not in list(cls.local_instance_dict_set()):
+                    error_methods.append(name)
+                    continue
+
+                method = getattr(cls, name)
+
+            else:
+                method = name_or_method
+
+                try:
+                    name = __not_cache__[index] = method.__name__
+                except AttributeError:
+                    if method.__class__ is property:
+                        name = __not_cache__[index] = method.fget.__name__
+                    else:
+                        name = method
+
+                if method not in list(cls.local_instance_dict_set(v=True)):
+                    error_methods.append(name)
+                    continue
+
+            if not (callable(method) or method.__class__ in (property, classmethod)):
+                error_methods.append(name)
+                continue
+
+        if error_methods:
+            x = f'{cls.__module__}.{cls.__name__}'
+            e = error_methods if len(error_methods) > 1 else error_methods[0]
+            raise cls.NotCacheDefineError(
+                f'The "{__package__}" instance "{x}" has no method "{e}".'
+            )
+
+    @staticmethod
+    def delete_repeated(data: list):
+        index = len(data) - 1
+        while index > -1:
+            offset = -1
+            while index + offset > -1:
+                if data[index + offset] == data[index]:
+                    del data[index]
+                    break
+                else:
+                    offset -= 1
+            index -= 1
+
+    def local_instance_dict_set(cls, baseclass=None, *, v: bool = False):
+        cur_cls: GqylpyCache or type = baseclass or cls
+
+        if cur_cls.__class__ is GqylpyCache:
+            yield from cur_cls.__dict__.values() if v else cur_cls.__dict__
+            yield from cur_cls.local_instance_dict_set(cur_cls.__base__)
+
+    class NotCacheDefineError(Exception):
+        __module__ = __package__
+
 
 class ClassMethodCaller:
+
+    def __new__(cls, cls_: GqylpyCache, ins, sget, name: str):
+        method = getattr(cls_, name)
+
+        if method.__class__ is property:
+            cache_pool: dict = sget('__cache_pool__')
+            try:
+                x = cache_pool[name]
+            except KeyError:
+                x = cache_pool[name] = sget(name)
+            return x
+
+        return object.__new__(cls)
 
     def __init__(self, cls: GqylpyCache, ins, sget, name: str):
         self.cls = cls
@@ -65,13 +147,10 @@ class ClassMethodCaller:
 
         try:
             x = cache_pool[key]
-            print(f'get cache: {self.name}, cls: {self.cls}')
-            return x
         except KeyError:
-            print(f'create cache: {self.name}, cls: {self.cls}')
-            x = self.sget(self.name)(*a, **kw)
-            cache_pool[key] = x
-            return x
+            x = cache_pool[key] = self.sget(self.name)(*a, **kw)
+
+        return x
 
     def __str__(self):
         return f'{ClassMethodCaller.__name__}(' \
@@ -80,18 +159,21 @@ class ClassMethodCaller:
 
 
 def __getattribute__(cls: GqylpyCache):
-    def inner(ins: cls, name: str):
+    def inner(ins: cls, attr: str):
         sget = super(cls, ins).__getattribute__
 
-        if name in ('__cache_pool__', '__not_cache__') or name not in cls.__dict__:
-            print(f'nonsupport cache: {name}, cls: {cls.__name__}, ins: {ins}')
-            return sget(name)
+        if (
+                attr in ('__cache_pool__', '__not_cache__') or
+                attr in sget('__not_cache__') or
+                attr not in cls.__dict__
+        ):
+            return sget(attr)
 
-        if name in cls.__not_cache__:
-            print(f'not cache: {name}, cls: {cls.__name__}, ins: {ins}')
-            return sget(name)
+        value = getattr(cls, attr)
 
-        print(f'cacheable: {name}, cls: {cls.__name__}, ins: {ins}')
-        return ClassMethodCaller(cls, ins, sget, name)
+        if not (callable(value) or value.__class__ in (property, classmethod)):
+            return sget(attr)
+
+        return ClassMethodCaller(cls, ins, sget, attr)
 
     return inner
