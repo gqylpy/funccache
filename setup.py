8f0b699015894a2f7a16b1a148cd806fd24d9483
竹永康 <gqylpy@outlook.com>
2024-04-01 09:56:32 +0800

    2.0alpha1
    1.Note: This version is a refactored version and many changes are not backward compatible.
    2.The scheme of reconstructing the return value of the cache function, uses the secondary lock. Solve the lock contention problem when the function is called concurrently with different parameters.
    3.The return value of the method in the cache class supports setting the expiration time.
    4.Add a decorator named `count`, supports caching the return value of a function by number of calls.
    5.The decorator with cache expiration `expires` is renamed to `ttl`.
    6.The cache expiration time parameter can be passed in unit-time (such as "1h40m").
    7.Fix issue: When an exception occurs in the call to the cached object, subsequent successive calls always raise `KeyError: '__return__'`. #2
    8.Fix issue: Concurrent calls when the return value of the function has not been cached or has expired may repeat the execution of the function. #4
    9.The `__getattribute__` method is no longer silently overridden, and an exception is thrown directly if it exists.
    10.Internal code adds type annotations, as well as a lot of optimization and refactoring.
    11.Python3.6 and 3.7 are no longer supported.
    
    1.注意：此版本为重构版本，大量改动不向下兼容。
    2.重构缓存函数返回值的方案，现采用二级锁。解决以不同参数并发调用函数时的锁竞争问题。
    3.缓存类中方法的返回值支持设置过期时间。
    4.新增装饰器 `count`，支持按调用次数缓存函数的返回值。
    5.带缓存有效期的装饰器 `expires` 更名为 `ttl`。
    6.缓存过期时间的参数支持按单位时间（如 "1h40m"）的方式传入。
    7.修复问题：当被缓存的对象调用出现异常后，后续的连续调用始终引发 `KeyError: '__return__'`。#2
    8.修复问题：当函数返回值还未被缓存或已过期时的并发调用可能重复执行函数。#4
    9.不再静默覆盖 `__getattribute__` 方法，如果它存在，将直接抛出异常。
    10.内部代码添加类型注解，以及大量优化和重构。
    11.Python3.6 和 3.7 版本不再受支持。

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     long_description=open('README.md', encoding='utf8').read(),
     long_description_content_type='text/markdown',
     packages=[i.__name__],
-    python_requires='>=3.6, <4',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -32,12 +32,11 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Topic :: Artistic Software',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12'
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13'
     ]
 )
